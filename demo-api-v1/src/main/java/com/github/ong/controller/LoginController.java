5039e6f23f73353ba0f92c23c2033bb04cbbe8e1
vergil-ong <865208597@qq.com>
2023-07-13 18:19:51 +0800

    封装请求

--- demo-api-v1/src/main/java/com/github/ong/controller/LoginController.java
+++ demo-api-v1/src/main/java/com/github/ong/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.github.ong.util.LogUtil;
 import com.github.ong.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @RequestMapping("/user")
-    public ResultVo loginUser(LoginQo loginQo) {
+    public ResultVo loginUser(@RequestBody LoginQo loginQo) {
         log.info("loginQo is {}", LogUtil.logObject(loginQo));
         String password = loginQo.getPassword();
         if (StringUtils.equals(password, "2")) {
